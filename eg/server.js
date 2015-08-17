41e461a88556745a8bb76b1796cbfb70157341fb
Rick Waldron <waldron.rick@gmail.com>
2015-08-17 15:10:44 -0400

    Events: implement "up", "down", "move"

--- eg/server.js
+++ eg/server.js
@@ -34,37 +34,81 @@ var io = socket.listen(app);
 var board = new five.Board();
 
 board.on("ready", function() {
-  var touch = new DSTouch();
+  var screen = new DSTouch();
   var previous = { x: null, y: null };
   var isDrawing = false;
   var socket = null;
 
-  touch.on("data", function() {
-    var data = { x: this.x, y: this.y };
-
+  function transmit(type, data) {
     if (!socket) {
       return;
     }
+    previous.x = data.x;
+    previous.y = data.y;
 
-    if (isDrawing) {
-      if (!isTouching(data)) {
-        isDrawing = false;
-        // Use the previous point instead of the current
-        // to avoid sending a garbage point (eg. [1023, 1023])
-        socket.emit("up", previous);
-      } else {
-        socket.emit("move", data);
-        previous.x = data.x;
-        previous.y = data.y;
-      }
-    } else {
-      if (!isDrawing && isTouching(data)) {
-        isDrawing = true;
-        socket.emit("down", data);
-        socket.emit("move", data);
-      }
-    }
+    socket.emit(type, data);
+  }
+
+  screen.on("move", function() {
+    console.log("move:", { x: this.x, y: this.y });
+    transmit("touch:move", { x: this.x, y: this.y });
   });
+
+  screen.on("down", function() {
+    console.log("down:", { x: this.x, y: this.y });
+    transmit("touch:down", { x: this.x, y: this.y });
+  });
+
+  screen.on("up", function() {
+    transmit("touch:up", previous);
+  });
+
+  //   if (isDrawing) {
+  //     if (!isTouching(data)) {
+  //       isDrawing = false;
+  //       // Use the previous point instead of the current
+  //       // to avoid sending a garbage point (eg. [1023, 1023])
+  //       socket.emit("up", previous);
+  //     } else {
+  //       socket.emit("move", data);
+  //       previous.x = data.x;
+  //       previous.y = data.y;
+  //     }
+  //   } else {
+  //     if (!isDrawing && isTouching(data)) {
+  //       isDrawing = true;
+  //       socket.emit("down", data);
+  //       socket.emit("move", data);
+  //     }
+  //   }
+  // });
+
+  // touch.on("data", function() {
+  //   var data = { x: this.x, y: this.y };
+
+  //   if (!socket) {
+  //     return;
+  //   }
+
+  //   if (isDrawing) {
+  //     if (!isTouching(data)) {
+  //       isDrawing = false;
+  //       // Use the previous point instead of the current
+  //       // to avoid sending a garbage point (eg. [1023, 1023])
+  //       socket.emit("up", previous);
+  //     } else {
+  //       socket.emit("move", data);
+  //       previous.x = data.x;
+  //       previous.y = data.y;
+  //     }
+  //   } else {
+  //     if (!isDrawing && isTouching(data)) {
+  //       isDrawing = true;
+  //       socket.emit("down", data);
+  //       socket.emit("move", data);
+  //     }
+  //   }
+  // });
 
   io.sockets.on("connection", function(connection) {
     console.log("connected");
--- index.js
+++ index.js
@@ -18,18 +18,43 @@ module.exports = function(five) {
       var address = opts.address || 0x04;
 
       var state = {
-        x: 0,
-        y: 0,
+        pending: false,
+        touching: false,
+        x: 1023,
+        y: 1023,
       };
 
       this.io.i2cConfig(opts);
 
       this.io.i2cRead(address, 4, function(data) {
+        var present = "";
         var x = five.Fn.int16(data[0], data[1]);
         var y = five.Fn.int16(data[2], data[3]);
 
         if (x !== state.x || y !== state.y) {
           this.emit("change", { x: x, y: y });
+        }
+
+        if (x === 1023 && y === 1023 && state.touching) {
+          state.touching = false;
+          this.emit("up", { x: state.x, y: state.y });
+        }
+
+        if (x !== 1023 && y !== 1023) {
+          if (!state.touching) {
+            // The stylus is down, but we don't want
+            // the very first reading. This will prevent points
+            // appearing as though they are still off the surface.
+            state.pending = true;
+          } else {
+            if (state.pending) {
+              state.pending = false;
+              this.emit("down", { x: x, y: y });
+            }
+            this.emit("move", { x: x, y: y });
+          }
+
+          state.touching = true;
         }
 
         state.x = x;
