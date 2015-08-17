a93944a00bb6ba6f6d47554c595b2af9135d3b3b
Rick Waldron <waldron.rick@gmail.com>
2015-08-17 15:30:18 -0400

    Ploma server cleanup

--- eg/server.js
+++ eg/server.js
@@ -36,7 +36,6 @@ var board = new five.Board();
 board.on("ready", function() {
   var screen = new DSTouch();
   var previous = { x: null, y: null };
-  var isDrawing = false;
   var socket = null;
 
   function transmit(type, data) {
@@ -45,17 +44,14 @@ board.on("ready", function() {
     }
     previous.x = data.x;
     previous.y = data.y;
-
     socket.emit(type, data);
   }
 
   screen.on("move", function() {
-    console.log("move:", { x: this.x, y: this.y });
     transmit("touch:move", { x: this.x, y: this.y });
   });
 
   screen.on("down", function() {
-    console.log("down:", { x: this.x, y: this.y });
     transmit("touch:down", { x: this.x, y: this.y });
   });
 
@@ -63,61 +59,9 @@ board.on("ready", function() {
     transmit("touch:up", previous);
   });
 
-  //   if (isDrawing) {
-  //     if (!isTouching(data)) {
-  //       isDrawing = false;
-  //       // Use the previous point instead of the current
-  //       // to avoid sending a garbage point (eg. [1023, 1023])
-  //       socket.emit("up", previous);
-  //     } else {
-  //       socket.emit("move", data);
-  //       previous.x = data.x;
-  //       previous.y = data.y;
-  //     }
-  //   } else {
-  //     if (!isDrawing && isTouching(data)) {
-  //       isDrawing = true;
-  //       socket.emit("down", data);
-  //       socket.emit("move", data);
-  //     }
-  //   }
-  // });
-
-  // touch.on("data", function() {
-  //   var data = { x: this.x, y: this.y };
-
-  //   if (!socket) {
-  //     return;
-  //   }
-
-  //   if (isDrawing) {
-  //     if (!isTouching(data)) {
-  //       isDrawing = false;
-  //       // Use the previous point instead of the current
-  //       // to avoid sending a garbage point (eg. [1023, 1023])
-  //       socket.emit("up", previous);
-  //     } else {
-  //       socket.emit("move", data);
-  //       previous.x = data.x;
-  //       previous.y = data.y;
-  //     }
-  //   } else {
-  //     if (!isDrawing && isTouching(data)) {
-  //       isDrawing = true;
-  //       socket.emit("down", data);
-  //       socket.emit("move", data);
-  //     }
-  //   }
-  // });
-
   io.sockets.on("connection", function(connection) {
-    console.log("connected");
     socket = connection;
   });
 
   exec("open http://localhost:8080/ploma-app/index.html");
 });
-
-function isTouching(point) {
-  return point.x !== 1023 && point.y !== 1023
-}
